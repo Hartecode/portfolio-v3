908c4a06152dabe9f6ee3deaee99588fec9f978b
Sean Harte <>
2020-03-18 08:55:37 -0400

    made star component

---
+++ src/components/StarField/StarField.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './StarField.css'
+
+const StarField = () =>  (
+  <div class="site-background" role="img" aria-label="night sky">
+    <div class="stars"></div>
+    <div class="twinkling"></div>
+    <div class="clouds"></div>
+  </div>
+);
+
+export default StarField
