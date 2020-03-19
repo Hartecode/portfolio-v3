dd7269898304e7fcb0594847212223f49a13776d
Sean Harte <>
2020-03-19 08:49:32 -0400

    updated project data file

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -1,4 +1,4 @@
-export const projectData = [
+const projectData = [
   {
       name: 'CollaBoard',
       image: {
@@ -8,7 +8,8 @@ export const projectData = [
       github: 'https://github.com/Hartecode/collab-board',
       website: 'http://www.collaboard.co/',
       skills: ['ES6', 'React', 'Redux', 'Express', 'Node.JS', 'MongoDB'],
-      description: 'CollaBoard is where a user can post a project they are seeking collaboration on and also search for a project where they can be a co-collaborator. The user can deny or approve any requests they receive. The user can also view their owned and collab projects.'
+      description: 'CollaBoard is where a user can post a project they are seeking collaboration on and also search for a project where they can be a co-collaborator. The user can deny or approve any requests they receive. The user can also view their owned and collab projects.',
+      yearDeveloped: 2018
   },
   {
       name: 'Baby Steps',
@@ -19,7 +20,8 @@ export const projectData = [
       github: 'https://github.com/Hartecode/baby-steps',
       website: 'http://www.babysteps.space/',
       skills: ['jQuery', 'AJAX', 'Express', 'Node.JS', 'MongoDB'],
-      description: 'Babies experience so much exciting growth very quickly. Baby Steps allows the user to log these different milestones. Users can log all of the special moments each of their children has.'
+      description: 'Babies experience so much exciting growth very quickly. Baby Steps allows the user to log these different milestones. Users can log all of the special moments each of their children has.',
+      yearDeveloped: 2018
   },
   {
       name: 'Crypto Movers',
@@ -30,6 +32,14 @@ export const projectData = [
       github: 'https://github.com/Hartecode/cryptomovers',
       website: 'http://www.cryptomovers.icu/',
       skills: ['JavaScript','jQuery', 'AJAX'],
-      description: 'This app ranks 50 cryptocurrencies by percentage change. The user can click on a currency or type the name of a cryptocurrency in the search bar to obtain the symbol, current price, percentage change, market cap, volume, and open price in the past 24 hours. The user can also click on the articles on the right on desktop screens or on the button on mobile screens for the latest in crypto news.'
+      description: 'This app ranks 50 cryptocurrencies by percentage change. The user can click on a currency or type the name of a cryptocurrency in the search bar to obtain the symbol, current price, percentage change, market cap, volume, and open price in the past 24 hours. The user can also click on the articles on the right on desktop screens or on the button on mobile screens for the latest in crypto news.',
+      yearDeveloped: 2018
   }
 ];
+
+const projectPageData = {
+    pageName: 'Projects',
+    projects: projectData
+}
+
+export default projectPageData
