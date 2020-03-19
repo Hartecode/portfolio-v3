813277fd25421c05922f2b61f23893e35e2b6ccd
Sean Harte <>
2020-03-19 08:48:58 -0400

    added className

--- src/components/StarField/StarField.js
+++ src/components/StarField/StarField.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './StarField.css'
 
 const StarField = () =>  (
-  <div class="site-background" role="img" aria-label="night sky">
-    <div class="stars"></div>
-    <div class="twinkling"></div>
-    <div class="clouds"></div>
+  <div className="site-background" role="img" aria-label="night sky">
+    <div className="stars"></div>
+    <div className="twinkling"></div>
+    <div className="clouds"></div>
   </div>
 );
 
