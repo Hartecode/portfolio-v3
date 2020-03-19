822f8b983bf18da6a60ea0bd682b689ea2a72653
Sean Harte <>
2020-03-19 08:46:14 -0400

    adding laning component to app component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Layout from './components/layout/layout';
 import StarField from './components/StarField/StarField';
+import Landing from './components/Landing/Landing';
 
 function App() {
   return (
     <Layout>
       <StarField></StarField>
+      <Landing></Landing>
     </Layout>
   );
 }
