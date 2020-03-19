05b545ca535f64f4134750a121c1d52f3bfa6ef6
Sean Harte <>
2020-03-19 16:08:26 -0400

    added bio section

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import './Landing.css';
-import Greeting from '../Greeting/Greeting';
-import greetingData from '../../utils/greetingData';
+import React from 'react'
+import './Landing.css'
+import Greeting from '../Greeting/Greeting'
+import greetingData from '../../utils/greetingData'
+import bioPageData from '../../utils/bioData'
+import BioSection from '../BioSection/BioSection'
 
 
-function Landing() {
+function Landing({bioRef}) {
   return (
-    <main>
+    <main className="main-container">
       <Greeting {...greetingData}></Greeting>
-      <section></section>
+      <BioSection bioPageData={bioPageData} areaRef={bioRef}></BioSection>
       <section></section>
     </main>
   );
 }
 
-export default Landing;
+export default Landing
