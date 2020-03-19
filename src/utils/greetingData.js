44559a28acca773108591f4731e3dcaf0a6e8855
Sean Harte <>
2020-03-19 08:52:23 -0400

    updated greeting data

---
+++ src/utils/greetingData.js
@@ -0,0 +1,32 @@
+const greetingData = {
+  greeting: "Hi, I'm Sean Harte",
+  title: 'A Full Stack developer!',
+  icons: [
+    {
+      label: "Sean Harte's GitHub",
+      link: 'https://github.com/Hartecode',
+      code: '&#xf09b;',
+      name: 'GitHub'
+    },
+    {
+      label: "Sean Harte's LinkedIn",
+      link: 'https://www.linkedin.com/in/seanharte/',
+      code: '&#xf30c;',
+      name: 'LinkedIn'
+    },
+    {
+      label: "Sean Harte's Resume",
+      link: '/seanharteResume.pdf',
+      code: '&#xf1c1;',
+      name: 'Résumé'
+    },
+    {
+      label: "Sean Harte's Email",
+      link: 'mailto:seanharte@live.com',
+      code: '&#xe800;',
+      name: 'Email'
+    }
+  ]
+};
+
+export default greetingData;
