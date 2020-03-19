3a163f1740d14134e198682224503396db7ca018
Sean Harte <>
2020-03-19 17:58:13 -0400

    added proejct section component to landing componet

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,14 +4,15 @@ import Greeting from '../Greeting/Greeting'
 import greetingData from '../../utils/greetingData'
 import bioPageData from '../../utils/bioData'
 import BioSection from '../BioSection/BioSection'
+import ProjectSection from '../ProjectSection/ProjectSection'
+import projectPageData from '../../utils/projectsData'
 
-
-function Landing({bioRef}) {
+function Landing({bioRef, projRef}) {
   return (
     <main className="main-container">
       <Greeting {...greetingData}></Greeting>
       <BioSection bioPageData={bioPageData} areaRef={bioRef}></BioSection>
-      <section></section>
+      <ProjectSection projectPageData={projectPageData} areaRef={projRef}></ProjectSection>
     </main>
   );
 }
